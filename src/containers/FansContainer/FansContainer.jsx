d238040fd0df40f2356e673cd370c778bf4e9a8b
Spencer Connelly <spenconnelly@gmail.com>
2020-07-26 01:47:16 -0400

    Fix linting error.

--- src/containers/FansContainer/FansContainer.jsx
+++ src/containers/FansContainer/FansContainer.jsx
@@ -45,7 +45,7 @@ function FansContainer(props) {
                 width="100%"
                 height="400"
                 src="https://www.youtube.com/embed/WHEthsO7T7w"
-                frameborder="0"
+                frameBorder="0"
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen
             />
